1fe9f59a236ccebca690f9d894054b1d92ae2186
mikkjal <mikkjaln@gmail.com>
2023-06-03 23:53:47 +0100

    fix github link

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -42,6 +42,6 @@ export default defineConfig({
 				],
 			},
 		],
-		socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }],
+		socialLinks: [{ icon: 'github', link: 'https://github.com/mikkjal/vtbl' }],
 	},
 });
